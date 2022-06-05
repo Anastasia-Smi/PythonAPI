691ce46dcd4ba96520ad4a9fc1c5257c57b8a613
Anastasia Smirnov <a.kurdeka@gmail.com>
2022-06-05 11:59:29 -0400

    ex.11

--- tests/test_ex_11.py
+++ tests/test_ex_11.py
@@ -4,20 +4,11 @@ from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 def test_ex_11(self, BaseCase):
-    data = {
-        'email': 'vincotov@example.com',
-        'password': '1234'
-    }
-    url = "https://playground.learnqa.ru/api/homework_cookie"
-    response = requests.post(url, data)
-    cookie = get_cookie(response, "auth_sid")
-    print (cookie)
 
-    Assertions.assert_json_value_by_name(
-        response,
-        "user_id",
-        cookie,
-        "Cookie is not available"
-    )
+    url = "https://playground.learnqa.ru/api/homework_cookie"
+    response = requests.get(url)
+    a = print(response.cookies)
+    assert (a in response.cookies)
+
 
 
